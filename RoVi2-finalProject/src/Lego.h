ff2a74f0d1e8f69a25a9c6ea0bb2be861222b413
Javakin <danielharaldson@yahoo.com>
2018-04-14 17:57:32 +0200

    rydet lidt op i namespaces

--- RoVi2-finalProject/src/Lego.h
+++ RoVi2-finalProject/src/Lego.h
@@ -7,32 +7,13 @@
 
 
 
-using namespace rw::common;
-using namespace rw::graphics;
-using namespace rw::kinematics;
-using namespace rw::loaders;
-using namespace rw::models;
-using namespace rw::sensor;
-using namespace rw::math;
 
-using namespace rwlibs::opengl;
-using namespace rwlibs::simulation;
-
-using namespace std;
-using namespace rws;
-using namespace cv;
 
 
 
 class Lego {
 
 public:
-    Lego(rw::common::LogWriter &lout, double f):log(lout),focal(f){}
-    Lego(rwlibs::opengl::RenderImage *_textureRender,rw::common::LogWriter &lout,double f):log(lout),focal(f){
-        //setup texture render
-        this->_textureRender = _textureRender;
-        //lout << "I am here" << endl;
-    }
 
 private:
 
